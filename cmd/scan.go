dbbc244b238413edadc3c9de4d61f6fe5b056002
Dom Goodwin <git@dgood.win>
2023-06-27 23:25:13 +0100

    fix dockerfile

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,19 +1,17 @@
 package cmd
 
 import (
-	"github.com/domgoodwin/bookscan/barcode"
 	"github.com/spf13/cobra"
 )
 
-
 func init() {
 	rootCmd.AddCommand(scanCmd)
-  }
-  
-  var scanCmd = &cobra.Command{
+}
+
+var scanCmd = &cobra.Command{
 	Use:   "scan",
 	Short: "Listen for books to scan",
 	Run: func(cmd *cobra.Command, args []string) {
-	  barcode.WaitForScan()
+		//   barcode.WaitForScan()
 	},
-  }
+}
